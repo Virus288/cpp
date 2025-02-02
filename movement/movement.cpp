641f3daa99f8e6ab2f3dd130d2e024c3d6126871
jakub <kiszczyckuba20@wp.pl>
2025-02-02 22:41:57 +0100

    Init

---
+++ movement/movement.cpp
@@ -0,0 +1,10 @@
+#include "movement.h"
+#include <iostream>
+
+using std::cout;
+
+int Movement::move(string location) {
+    cout << "User is moving to the " << location;
+    return 0;
+};
+
---
+++ movement/movement.h
@@ -0,0 +1,9 @@
+#include <string>
+
+using std::string;
+
+class Movement {
+    public: int move(string location);
+};
+
+#pragma once  /* This line makes sure that file is used only once */
